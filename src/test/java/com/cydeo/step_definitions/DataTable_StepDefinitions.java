3ea10976abd123db1626c66f2869f3a6c15c06bc
cholponai <cholpondzhumabaeva@gmail.com>
2022-07-18 11:58:02 +0200

    day17 Dropdown datatable Task

--- src/test/java/com/cydeo/step_definitions/DataTable_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/DataTable_StepDefinitions.java
@@ -1,9 +1,14 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.DropdownsPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataTable_StepDefinitions {
@@ -14,13 +19,29 @@ public class DataTable_StepDefinitions {
         System.out.println(fruits.get(1));
     }
 
+    DropdownsPage dropdownsPage= new DropdownsPage();
+
+
     @Given("User is on the dropdowns page of practice tool")
     public void user_is_on_the_dropdowns_page_of_practice_tool() {
-        Driver.getDriver().get("https://practice.cydeo.com/dropdown ");
+        Driver.getDriver().get("https://practice.cydeo.com/dropdown");
     }
     @Then("User should see below info in month dropdown")
     public void user_should_see_below_info_in_month_dropdown(List<String>expectedMonth) {
-        System.out.println("expectedMonth = " + expectedMonth);
+        Select select = new Select(dropdownsPage.monthDropdown);
+        List<WebElement>actualOptionsAsWebElement = select.getOptions();
+
+        List<String>actualOptionsAsString = new ArrayList<>();
+
+        for (WebElement each : actualOptionsAsWebElement) {
+            actualOptionsAsString.add(each.getText());
+        }
+
+        Assert.assertEquals(expectedMonth,actualOptionsAsString);
+
+        //Assert will check the size of the Lists first.If it is matching ,it will check content 1 by 1.
+
+
 
     }
 }
